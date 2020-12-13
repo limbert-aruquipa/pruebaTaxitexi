2bbd64c596111bc08a9132fc8213d300ccdec7ce
limbertAruquipa <limbert0226@gmail.com>
2020-12-12 23:47:29 -0400

    se agrego la funcionalidad de buscar usuarios y link para ver la pagina de github del mismo

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,6 @@
 var request = new XMLHttpRequest()
-
 var form = document.getElementById("formMenu")
 var statusHTML='';
-
 form.addEventListener('submit', function(e){
     e.preventDefault()
     var search= document.getElementById("txtBuscar").value
@@ -12,19 +10,7 @@ form.addEventListener('submit', function(e){
         console.log(data)
         document.getElementById("divResul").innerHTML= '<a target="blank"      <a href="https://www.github.com/'+nombreReal+'">         <img src="'+ data.avatar_url + '"/>                       </a>'
         document.getElementById("linkGithub").innerHTML='<a target="blank"      <a href="https://www.github.com/'+nombreReal+'">Ver página de Github</a>'
-        document.getElementById("repositorios").innerHTML='<a target="blank"      <a href="https://www.github.com/'+nombreReal+'">Ver Repositorios</a>'
         document.getElementById("txtNombre").innerHTML='<h1>Nombre de Usuario: '+nombreReal+'</h1>'
-    
-        //document.getElementById("divResul").innerHTML='<a href="https://www.github.com/'+nombreReal+'">'  
-       //'<a target='+_blank+'    
-       //<a href="https://www.github.com/'+nombreReal+'">     
-       //'<img src="'+ data.avatar_url + '"/>'  
-       //'<a href="https://www.github.com/'+nombreReal+'">         <img src="'+ data.avatar_url + '"/> '
-       //'<a href="https://www.github.com/'+nombreReal+'">'
-       //'<a target="blank"  </a>'
-   
    })
-
-
 })
 
